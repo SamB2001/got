9b61ad8a971b2254b2df5c7b3788fd3e7e4894c0
Sam Bischoff <sam.bischoff2001@gmail.com>
2022-05-25 12:30:29 -0600

    Jamies first attack

--- codealong2.js
+++ codealong2.js
@@ -1,9 +1,24 @@
-
+// jon stats
 let jonSnowAttack = 25;
-let jamieLannisterAttack = 35;
+let jonSnowHealth = 100;
+let jonSnowDefense = 0;
+
+//jamie stats
+let jamieLannisterAttack = 107;
 
 if (jonSnowAttack > jamieLannisterAttack) {
     console.log('Jon Snow has better attack than Jamie Lannister');
-} else {
+} else if (jonSnowAttack < jamieLannisterAttack) {
     console.log('Jamie Lannister has better attack than Jon Snow')
 }
+
+else{
+    console.log('Jon Snow and Jamie Lannister have the same attack')
+}
+
+    if (jonSnowHealth <= jamieLannisterAttack) {
+        console.log('Jon Snow has been slain');
+    } else {
+        jonSnowHealth -= jamieLannisterAttack;
+        console.log(`Jon Snow's health is down to ${jonSnowHealth}`)
+    }
